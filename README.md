6c57f9a58d8c7bf76413d6d6c0cbd68b557de930
James Hu <hello@james.hu>
2021-11-01 16:19:51 -0700

    Cleanup

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # sublime-0x0
 
-This package helps post selected text or the entire file from Sublime Text to 0x0.
+This package helps post selected text or the entire file within Sublime Text to [0x0](https://0x0.st). The resulting URL is then copied to the clipboard.
 
-Note that most of this work was built on top of [pastebin-sublime-plugin](https://github.com/Paaskehare/pastebin-sublime-plugin).
+## Usage
+
+Activate the Command Palette and search for "0x0" to do the deed.
+
+## Thanks
+
+Most of this work was built on top of [pastebin-sublime-plugin](https://github.com/Paaskehare/pastebin-sublime-plugin).
--- sublime-0x0.py
+++ sublime-0x0.py
@@ -3,112 +3,112 @@ import string, random, os.path
 
 # Python 2
 try:
-    import urllib2
+  import urllib2
 
 # Python 3
 except ImportError:
-    import urllib.request as urllib2
-
-HOSTNAME = 'https://0x0.st'
-POST_FILE_FIELD = 'file'
+  import urllib.request as urllib2
 
 class FilePart:
-    CONTENT_TRANSFER_ENCODING = 'utf-8'
+  CONTENT_TRANSFER_ENCODING = 'utf-8'
 
-    def __init__(self, name, filename, body, boundary):
-      self.name = name
-      self.filename = filename
-      self.body = body
-      self.boundary = boundary
-      self.headers = {
-        # Needs to be text/plain content type otherwise the file will get downloaded by browser
-        'Content-Type': 'text/plain;charset=utf-8;',
+  def __init__(self, name, filename, body, boundary):
+    self.name = name
+    self.filename = filename
+    self.body = body
+    self.boundary = boundary
+    self.headers = {
+      # Needs to be text/plain content type otherwise the file will get downloaded by browser
+      'Content-Type': 'text/plain;charset=utf-8;',
 
-        'Content-Disposition': 'form-data; name="{0}"; filename="{1}"'.format(self.name, self.filename),
-        'Content-Transfer-Encoding': self.CONTENT_TRANSFER_ENCODING,
-      }
+      'Content-Disposition': 'form-data; name="{0}"; filename="{1}"'.format(self.name, self.filename),
+      'Content-Transfer-Encoding': self.CONTENT_TRANSFER_ENCODING,
+    }
 
-    def get(self):
-        lines = []
-        lines.append('--' + self.boundary)
+  def get(self):
+    lines = []
+    lines.append('--' + self.boundary)
 
-        for key, val in self.headers.items():
-            lines.append('{0}: {1}'.format(key, val))
+    for key, val in self.headers.items():
+      lines.append('{0}: {1}'.format(key, val))
 
-        lines.append('')
-        lines.append(self.body)
-        lines.append('--{0}--'.format(self.boundary))
-        lines.append('')
+    lines.append('')
+    lines.append(self.body)
+    lines.append('--{0}--'.format(self.boundary))
+    lines.append('')
 
-        return lines
+    return lines
 
 class FileForm:
-    NEW_LINE = '\r\n'
+  NEW_LINE = '\r\n'
 
-    # Generate a random boundary
-    def _gen_boundary(self):
-        chars = string.ascii_lowercase + string.digits
-        return ''.join(random.choice(chars) for x in range(40))
+  # Generate a random boundary
+  def _gen_boundary(self):
+    chars = string.ascii_lowercase + string.digits
 
-    def __init__(self):
-        self.boundary = self._gen_boundary()
-        self._file = None
+    return ''.join(random.choice(chars) for x in range(40))
 
-    def file(self, filename, content):
-        self._file = FilePart(POST_FILE_FIELD, filename, content, self.boundary)
+  def __init__(self):
+    self.boundary = self._gen_boundary()
+    self._file = None
 
-    def get(self):
-        # file returns an array
-        content = self._file.get()
+  def file(self, filename, content):
+    # Needs to be submitted under 'file'. Other supported names are: url, shorten
+    self._file = FilePart('file', filename, content, self.boundary)
 
-        content_type = 'multipart/form-data; boundary=' + self.boundary
+  def get(self):
+    # File returns an array
+    content = self._file.get()
 
-        return content_type, self.NEW_LINE.join(content).encode(FilePart.CONTENT_TRANSFER_ENCODING)
+    content_type = 'multipart/form-data; boundary=' + self.boundary
 
+    return content_type, self.NEW_LINE.join(content).encode(FilePart.CONTENT_TRANSFER_ENCODING)
+
+# Can also be tested in console by running `view.run_command('post0x0')`
 class Post0x0Command(sublime_plugin.TextCommand):
+  def _get_file_name(self):
+    name = 'untitled'
 
-    def _get_file_name(self):
-        name = "untitled"
-        try:
-            name = os.path.split(self.view.file_name())[-1]
-        except AttributeError:
-            pass
-        except TypeError:
-            pass
-        return name
+    try:
+        name = os.path.split(self.view.file_name())[-1]
+    except AttributeError:
+        pass
+    except TypeError:
+        pass
 
-    def run(self, edit):
+    return name
 
-        # init an empty unicode string
-        content = u''
+  def run(self, edit):
+    # Init an empty unicode string
+    content = u''
 
-        # loop over the selections in the view:
-        for region in self.view.sel():
+    # Loop over the selections in the view
+    for region in self.view.sel():
+      if not region.empty():
+        # Be sure to insert a newline if we have multiple selections
+        if content:
+          content += FileForm.NEW_LINE
 
-            if not region.empty():
-                # be sure to insert a newline if we have multiple selections
-                if content:
-                    content += FileForm.NEW_LINE
-                content += self.view.substr(region)
+        content += self.view.substr(region)
 
-        # if we havent gotten data from selected text,
-        # we assume the entire file should be pasted:
-        if not content:
-            content += self.view.substr(sublime.Region(0, self.view.size()))
+    # If we havent gotten data from selected text, we assume the entire file should be pasted
+    if not content:
+      content += self.view.substr(sublime.Region(0, self.view.size()))
 
-        filename = self._get_file_name()
+    filename = self._get_file_name()
 
-        form = FileForm()
+    # Build our virtual file that will hold the content
+    form = FileForm()
+    form.file(filename = filename, content = content)
 
-        # insert the "fake" file
-        form.file(filename = filename, content = content)
+    content_type, body = form.get()
 
-        content_type, body = form.get()
+    # Post the file to 0x0
+    request = urllib2.Request(url = 'https://0x0.st', headers = { 'Content-Type': content_type }, data = body)
+    response = urllib2.urlopen(request).read().decode(FilePart.CONTENT_TRANSFER_ENCODING)
 
-        request = urllib2.Request(url=HOSTNAME, headers={'Content-Type': content_type}, data=body)
-        reply = urllib2.urlopen(request).read().decode(FilePart.CONTENT_TRANSFER_ENCODING)
-
-        sublime.set_clipboard(reply)
-        sublime.status_message("Paste: " + reply)
+    # Response is the URL to the content
+    sublime.set_clipboard(response)
+    sublime.status_message("Copied: {0}".format(response))
 
 
